49602ec27c865d9386b5fcaedef1b14f61246112
Guilherme Leandro Gomes Oliveira <90000784+Guilhermitao@users.noreply.github.com>
2022-06-22 22:06:19 -0300

    atualizacao

--- calculadora2/Program.cs
+++ calculadora2/Program.cs
@@ -1,54 +1,61 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace calculadora2
+namespace calculadoraavancada
 {
-    class Program
+    class Program2
     {
         static void Main(string[] args)
         {
-            string continuar;
-            int operacao;
-            double numero1, numero2;
-            double resultado1;
+            double numero_1, numero_2;
+            int num = 0, tab = 0;
+            string continua;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;//cor que ficará atras do texto
+            Console.ForegroundColor = ConsoleColor.White;//cor do texto
             do
             {
-                Console.WriteLine("digite um numero");
-                numero1 = double.Parse(Console.ReadLine());
-                Console.WriteLine("digite outro numero");
-                numero2 = double.Parse(Console.ReadLine());
-
-                Console.WriteLine("escolha uma operacao [1=soma, 2=subtracao, 3=multiplicacao, 4=divisao]");
-                operacao = int.Parse(Console.ReadLine());
-                switch (operacao)
+                Console.WriteLine("digite o primeiro numero");
+                numero_1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("digite o segundo numero");
+                numero_2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("os numeros digitados sao " + numero_1 + " e " + numero_2);
+                Console.WriteLine("Escolha uma operação:\r\n1=Adicão.\r\n2=Subtração.\r\n3=Multiplicação.\r\n4=Divisão.");
+                /* \r\n = <br> */
+                num = int.Parse(Console.ReadLine());
+                //Console.ReadKey();
+                switch (num)
                 {
                     case 1:
-                        resultado1 = (numero1 + numero2);
-                        Console.WriteLine("o resultado da soma e");
-                        Console.WriteLine(resultado1);
+                        Console.WriteLine("Adicão");
+                        Console.WriteLine("o resultado da soma é: "
+                            + numero_1 + " + " + numero_2 + " = " + (numero_1 + numero_2));
                         break;
                     case 2:
-                        resultado1 = (numero1 - numero2);
-                        Console.WriteLine("o resultado da subtracao e");
-                        Console.WriteLine(resultado1);
+                        Console.WriteLine("Subtração");
+                        Console.WriteLine("o resultado da subtração é: "
+                            + numero_1 + " - " + numero_2 + " = " + (numero_1 - numero_2));
                         break;
                     case 3:
-                        resultado1 = (numero1 * numero2);
-                        Console.WriteLine("o resultado da multiplicacao e");
-                        Console.WriteLine(resultado1);
+                        Console.WriteLine("Multiplicação");
+                        Console.WriteLine("o resultado da multiplicação é: "
+                            + numero_1 + " X " + numero_2 + " = " + (numero_1 * numero_2));
                         break;
                     case 4:
-                        resultado1 = (numero1 / numero2);
-                        Console.WriteLine("o resultado da divisao e");
-                        Console.WriteLine(resultado1);
+                        Console.WriteLine("Divisão");
+                        Console.WriteLine("o resultado da multiplicação é: "
+                            + numero_1 + " / " + numero_2 + " = " + (numero_1 / numero_2));
                         break;
-                }
-                Console.WriteLine("deseja fazer outra conta?aperte qualquer tecla para continuar[s=parar programa] ");
-                continuar = Console.ReadLine();
-            } while (continuar != "s" & continuar != "S");
-        }
-    }
-}
+                    default:
+                        Console.WriteLine("Opcao invalida!");
+                        break;
+                }//switch
+                Console.WriteLine("Quer continuar fazendo conta?");
+                Console.WriteLine("s=sim.\r\nQualquer botão = saida");
+                continua = Console.ReadLine();
+            } while (continua == "S" || continua == "s");// & = e; || = ou;
+        }//static void Main(string[] args)
+    }//class Program2
+}//namespace calculadoraavancada
